3c35cd41725b7d85b29ae10d9d87c26d9d87ee58
Billal <billal7chami@gmail.com>
2023-05-20 03:03:00 +0100

    Cinfigured linters and updated README file

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,28 +3,27 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe 'associations' do
-
-    it "should belong to a user" do
-        association = described_class.reflect_on_association(:user)
-        expect(association.macro).to eq(:belongs_to)
-      end
+    it 'should belong to a user' do
+      association = described_class.reflect_on_association(:user)
+      expect(association.macro).to eq(:belongs_to)
+    end
 
     it 'has many entities' do
-        association = described_class.reflect_on_association(:entities)
-        expect(association.macro).to eq(:has_and_belongs_to_many)
-      end
+      association = described_class.reflect_on_association(:entities)
+      expect(association.macro).to eq(:has_and_belongs_to_many)
     end
+  end
 
   describe 'validations' do
     let(:category) { Category.new(name: 'Groceries', icon: 'test.jpg') }
 
-    it "is invalid without a name" do
-        category = Category.new(name: 'Groceries')
-        expect(category).to be_invalid
-      end      
+    it 'is invalid without a name' do
+      category = Category.new(name: 'Groceries')
+      expect(category).to be_invalid
+    end
 
     it 'is invalid without an icon' do
-      category = Category.new(icon: "test.jpg")
+      category = Category.new(icon: 'test.jpg')
       category.icon = nil
       expect(category).to be_invalid
     end
